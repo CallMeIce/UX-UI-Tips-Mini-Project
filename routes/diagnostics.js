2a757a6698790753ae099fd0a275bcda4a65d2c0
Isaak Morales <ice.isaak97@gmail.com>
2022-04-14 12:44:00 -0400

    gotta finish project

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -4,11 +4,20 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
+  readFromFile('./db/diagnostics.json').then((data) => res.json(JSON.parse(data)));
+
   // TODO: Logic for sending all the content of db/diagnostics.json
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
+
+  // const {?, ?, ?} = req.body
+  // readFromFile('./db/diagnostics.json').then((data) =>
+  // JSON.parse(data)
+
+  //const to destructure keys/props off of req.body
+  //conditional for confirming necessary keys/var
   // TODO: Logic for appending data to the db/diagnostics.json file
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,16 @@ const express = require('express');
 // Import our modular routers for /tips and /feedback
 const tipsRouter = require('./tips');
 const feedbackRouter = require('./feedback');
-const diagnostics = require('./diagnostics')
-// TODO: import your diagnostics route
+const diagnosticRouter = require('./diagnostics')
+// X TODO: import your diagnostics route
 
 const app = express();
 
+
+//*localhost:3001/api
 app.use('/tips', tipsRouter);
 app.use('/feedback', feedbackRouter);
-// TODO: Initialize diagnostics route
+app.use('/diagnostics', diagnosticRouter)
+// X TODO: Initialize diagnostics route
 
 module.exports = app;
